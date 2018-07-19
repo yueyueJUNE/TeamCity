c1f994a9c40a1c789c4971abde76de961f67d6d2
yueyueJUNE <liuyue@liuyues-MacBook-Pro-2.local>
2018-07-19 00:30:27 -0400

    first commit

---
+++ TeamCity.Test/UnitTest1.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TeamCity.Controllers;
+
+namespace TeamCity.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Index()
+        {
+            //Arrange
+            HomeController homeController = new HomeController();
+            //Act
+            ViewResult result = homeController.Index() as ViewResult;
+            //Assertion
+            Assert.AreEqual("TEST", result.ViewData["Message"]);
+            
+        }
+
+        [TestMethod]
+        public void About()
+        {
+            //Arrange
+            HomeController homeController = new HomeController();
+            //Act
+            ViewResult result = homeController.About() as ViewResult;
+            //Assertion
+            Assert.AreEqual("Your application description page.", result.ViewData["Message"]);
+        }
+
+
+        [TestMethod]
+        public void Concat()
+        {
+            //Arrange
+            HomeController homeController = new HomeController();
+            //Act
+            ViewResult result = homeController.Contact() as ViewResult;
+            //Assertion
+            Assert.AreEqual("Your contact page.", result.ViewData["Message"]);
+        }
+    }
+}
